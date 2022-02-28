bbd6b49ac9e7d95ab64d2a1a974b9102d8b8c634
Zarisa <jannesari.zarisa@gmail.com>
2022-02-28 18:12:23 +0330

    add the view binding feature

--- app/src/main/java/com/zarisa/hw8_zahrajannesari_infoapp/FirstActivity.kt
+++ app/src/main/java/com/zarisa/hw8_zahrajannesari_infoapp/FirstActivity.kt
@@ -2,10 +2,14 @@ package com.zarisa.hw8_zahrajannesari_infoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zarisa.hw8_zahrajannesari_infoapp.databinding.ActivityFirstBinding
 
 class FirstActivity : AppCompatActivity() {
+    lateinit var binding:ActivityFirstBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_first)
+        binding= ActivityFirstBinding.inflate(layoutInflater)
+        val view=binding.root
+        setContentView(view)
     }
 }
