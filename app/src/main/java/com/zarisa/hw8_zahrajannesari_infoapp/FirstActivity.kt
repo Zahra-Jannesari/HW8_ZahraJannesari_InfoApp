c3692bdfdc1b3c1e91c0d047c95be9e22e7202cb
Zarisa <jannesari.zarisa@gmail.com>
2022-03-01 02:01:15 +0330

    just a rename

--- app/src/main/java/com/zarisa/hw8_zahrajannesari_infoapp/FirstActivity.kt
+++ app/src/main/java/com/zarisa/hw8_zahrajannesari_infoapp/FirstActivity.kt
@@ -5,19 +5,21 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.RadioGroup
+import androidx.core.text.isDigitsOnly
 import com.zarisa.hw8_zahrajannesari_infoapp.databinding.ActivityFirstBinding
 
 class FirstActivity : AppCompatActivity(), RadioGroup.OnCheckedChangeListener {
     lateinit var binding:ActivityFirstBinding
-    private var sharedpreferences: SharedPreferences? = null
+    private var sharedPreferences: SharedPreferences? = null
+    private var infoList= listOf(binding.textFieldFullName,binding.textFieldID,binding.textFieldBirthday
+                                ,binding.textFieldAddress,binding.textFieldZipCode)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityFirstBinding.inflate(layoutInflater)
         val view=binding.root
         setContentView(view)
-        sharedpreferences = getSharedPreferences("Information", Context.MODE_PRIVATE)
+        sharedPreferences = getSharedPreferences("Information", Context.MODE_PRIVATE)
         initViews()
-
     }
     private fun initViews(){
         binding.RadioGroupGender.setOnCheckedChangeListener(this)
@@ -38,11 +40,26 @@ class FirstActivity : AppCompatActivity(), RadioGroup.OnCheckedChangeListener {
     }
 
     private fun checkInfo(): Boolean {
-        TODO("Not yet implemented")
+        var result=true
+       infoList.forEach{
+           if(it.text.isBlank()) {
+               it.error ="تکمیل فیلد اجباری است!"
+               result= false
+           }
+       }
+        if(!binding.textFieldID.text.isDigitsOnly()&&binding.textFieldID.text.length!=10){
+            binding.textFieldID.error="کد ملی صحیح نمی باشد!"
+            result= false
+        }
+        if(!binding.textFieldZipCode.text.isDigitsOnly()){
+            binding.textFieldID.error="کد پستی صحیح نمی باشد!"
+            result= false
+        }
+        return result
     }
 
     override fun onCheckedChanged(p0: RadioGroup?, p1: Int) {
-        val editor = sharedpreferences?.edit()
+        val editor = sharedPreferences?.edit()
         when(p1){
             R.id.radioButton_Female -> {
                 editor?.putString("Gender", "Female")
