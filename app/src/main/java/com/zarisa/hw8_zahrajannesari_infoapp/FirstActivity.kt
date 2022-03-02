2f18e66d612b023dfdf8f11f72fa5a3b97e31041
Zarisa <jannesari.zarisa@gmail.com>
2022-03-02 16:54:49 +0330

    implement the firs activity completely

--- app/src/main/java/com/zarisa/hw8_zahrajannesari_infoapp/FirstActivity.kt
+++ app/src/main/java/com/zarisa/hw8_zahrajannesari_infoapp/FirstActivity.kt
@@ -1,18 +1,28 @@
 package com.zarisa.hw8_zahrajannesari_infoapp
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.core.text.isDigitsOnly
 import com.zarisa.hw8_zahrajannesari_infoapp.databinding.ActivityFirstBinding
+import java.util.regex.Pattern
 
+const val fullName="fullName"
+const val ID="Id"
+const val birthday="birthday"
+const val address="address"
+const val Gender="Gender"
+const val zipCode="zipCode"
+const val edit="Edit"
 class FirstActivity : AppCompatActivity(), RadioGroup.OnCheckedChangeListener {
-    lateinit var binding:ActivityFirstBinding
+    private lateinit var binding:ActivityFirstBinding
     private var sharedPreferences: SharedPreferences? = null
-    private var infoList= listOf(binding.textFieldFullName,binding.textFieldID,binding.textFieldBirthday
-                                ,binding.textFieldAddress,binding.textFieldZipCode)
+    private var infoList= mutableSetOf<EditText>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityFirstBinding.inflate(layoutInflater)
@@ -21,22 +31,57 @@ class FirstActivity : AppCompatActivity(), RadioGroup.OnCheckedChangeListener {
         sharedPreferences = getSharedPreferences("Information", Context.MODE_PRIVATE)
         initViews()
     }
+
     private fun initViews(){
-        binding.RadioGroupGender.setOnCheckedChangeListener(this)
-        binding.buttonRegister.setOnClickListener {
-            if(checkInfo()) {
-                saveInfoInShared()
-                goToSecondPage()
+        var editTime=intent.getBooleanExtra(edit,false)
+        if (sharedPreferences?.getString(fullName, "")!=""&&!editTime){
+            goToSecondPage()
+        }
+        if(editTime){
+            putDataForEdit()
+        }
+        else {
+            infoList = mutableSetOf(binding.textFieldFullName,binding.textFieldID,binding.textFieldBirthday,
+                                    binding.textFieldAddress,binding.textFieldZipCode)
+            binding.RadioGroupGender.setOnCheckedChangeListener(this)
+            binding.buttonRegister.setOnClickListener {
+                if (checkInfo()) {
+                    saveInfoInShared()
+                    goToSecondPage()
+                }
             }
         }
     }
 
+    private fun putDataForEdit() {
+        binding.textFieldFullName.setText(sharedPreferences?.getString(fullName,""))
+        binding.textFieldID.setText(sharedPreferences?.getString(ID,""))
+        binding.textFieldBirthday.setText(sharedPreferences?.getString(birthday,""))
+        binding.textFieldAddress.setText(sharedPreferences?.getString(address,""))
+        binding.textFieldZipCode.setText(sharedPreferences?.getString(zipCode,""))
+        when(sharedPreferences?.getString(Gender,"")){
+            "Female"->binding.radioButtonFemale.isChecked=true
+            "Male"->binding.radioButtonMale.isChecked=true
+        }
+    }
+
     private fun goToSecondPage() {
-        TODO("Not yet implemented")
+        val intent=Intent(this,SecondActivity::class.java)
+        startActivity(intent)
     }
 
     private fun saveInfoInShared() {
-        TODO("Not yet implemented")
+        val editor = sharedPreferences?.edit()
+        editor?.putString(fullName, binding.textFieldFullName.text.toString())
+        editor?.apply()
+        editor?.putString(ID, binding.textFieldID.text.toString())
+        editor?.apply()
+        editor?.putString(birthday, binding.textFieldBirthday.text.toString())
+        editor?.apply()
+        editor?.putString(address, binding.textFieldAddress.text.toString())
+        editor?.apply()
+        editor?.putString(zipCode, binding.textFieldZipCode.text.toString())
+        editor?.apply()
     }
 
     private fun checkInfo(): Boolean {
@@ -47,32 +92,49 @@ class FirstActivity : AppCompatActivity(), RadioGroup.OnCheckedChangeListener {
                result= false
            }
        }
-        if(!binding.textFieldID.text.isDigitsOnly()&&binding.textFieldID.text.length!=10){
-            binding.textFieldID.error="کد ملی صحیح نمی باشد!"
-            result= false
+        binding.textFieldID.text.let {
+
+            if (it.isNotBlank() && (!it.toString().hasOnlyDigits() || it.length != 10)) {
+                binding.textFieldID.error = "کد ملی صحیح نمی باشد!"
+                result = false
+            }
         }
-        if(!binding.textFieldZipCode.text.isDigitsOnly()){
-            binding.textFieldID.error="کد پستی صحیح نمی باشد!"
-            result= false
+        binding.textFieldZipCode.text.let{
+            if (it.isNotBlank() && !it.toString().hasOnlyDigits()) {
+                binding.textFieldZipCode.error = "کد پستی صحیح نمی باشد!"
+                result = false
+            }
+        }
+        if (sharedPreferences?.getString(Gender, "")==""){
+            Toast.makeText(this, "Enter Gender!", Toast.LENGTH_SHORT).show()
+            result=false
         }
         return result
     }
 
+    private fun String?.hasOnlyDigits() : Boolean = try {
+        if (this.isNullOrEmpty())
+            false
+        else Pattern.matches("[0-9]+", this)
+    } catch (e: Exception) {
+        e.printStackTrace()
+        false
+    }
     override fun onCheckedChanged(p0: RadioGroup?, p1: Int) {
         val editor = sharedPreferences?.edit()
         when(p1){
             R.id.radioButton_Female -> {
-                editor?.putString("Gender", "Female")
+                editor?.putString(Gender, "Female")
                 editor?.apply()
                 return
             }
             R.id.radioButton_Male ->{
-                editor?.putString("Gender","Male")
+                editor?.putString(Gender,"Male")
                 editor?.apply()
                 return
             }
         }
     }
-
 }
+
 
